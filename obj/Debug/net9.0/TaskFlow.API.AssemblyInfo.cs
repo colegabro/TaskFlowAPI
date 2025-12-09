a29905388b63ba9e5665768ed5431e0371d3146b
Nicolas Bandeira <nicolas.bandeira136@gmail.com>
2025-12-09 19:02:41 -0400

    Correção no swagger

--- obj/Debug/net9.0/TaskFlow.API.AssemblyInfo.cs
+++ obj/Debug/net9.0/TaskFlow.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskFlow.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cba94f8b723437d00b21d152329870256b07431")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskFlow.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskFlow.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
